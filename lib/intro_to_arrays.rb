6975e7cb932683229b0ca46168f38a1c8cb9eb6d
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-05 20:00:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,7 +2,6 @@ def instantiate_new_array
   my_new_array = []
   return my_new_array
 end
-
 def array_with_two_elements
   array_with_two_elements = ["Welcome to New York","two"]
 end
@@ -24,4 +23,3 @@ end
 def length_of_array(array)
   array.length
 end
-  
