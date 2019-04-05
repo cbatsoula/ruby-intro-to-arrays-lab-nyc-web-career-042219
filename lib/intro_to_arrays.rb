b7a56c0c059e32069f502ed2da6b869039570e5a
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-05 19:34:17 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,14 @@
+def instantiate_new_array
+  @my_new_array = []
+  return @my_new_array
+end
+
+def array_with_two_elements
+  @array_with_two_elements = ["Welcome to New York","two"]
+end
+def first_element(array)
+  array_with_two_elements[0]
+end
+def third_element(array)
+  array_with_two_elements[2]
+end
